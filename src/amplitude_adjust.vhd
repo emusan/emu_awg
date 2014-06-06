223e4c6aa737b3bb80a6b8f7ee612fa77ba03e2d
Tom McLeod <emus.attack@gmail.com>
2014-06-06 16:02:38 -0400

    Alter amplitude adjustment to remove some unneccessary warnings, still a few left to work out. The code should still work the same.

--- src/amplitude_adjust.vhd
+++ src/amplitude_adjust.vhd
@@ -40,12 +40,19 @@ end amplitude_adjust;
 
 architecture Behavioral of amplitude_adjust is
 
-	signal one_shift: unsigned(11 downto 0);
-	signal two_shift: unsigned(11 downto 0);
-	signal three_shift: unsigned(11 downto 0);
-	signal four_shift: unsigned(11 downto 0);
-	signal five_shift: unsigned(11 downto 0);
-	signal six_shift: unsigned(11 downto 0);
+	signal one_shift: unsigned(10 downto 0);
+	signal two_shift: unsigned(9 downto 0);
+	signal three_shift: unsigned(8 downto 0);
+	signal four_shift: unsigned(7 downto 0);
+	signal five_shift: unsigned(6 downto 0);
+	signal six_shift: unsigned(6 downto 0);
+	
+	signal one_shift_temp: unsigned(11 downto 0);
+	signal two_shift_temp: unsigned(11 downto 0);
+	signal three_shift_temp: unsigned(11 downto 0);
+	signal four_shift_temp: unsigned(11 downto 0);
+	signal five_shift_temp: unsigned(11 downto 0);
+	signal six_shift_temp: unsigned(11 downto 0);
 	
 begin
 
@@ -54,7 +61,7 @@ begin
 	begin
 		if(rising_edge(clk)) then
 			if adjust(5) = '1' then
-				one_shift <= unsigned(sine_in) srl 1;
+				one_shift <= (unsigned(sine_in) srl 1);
 			else 
 				one_shift <= (others => '0');
 			end if;
@@ -91,11 +98,18 @@ begin
 			if(adjust = "111111") then
 				sine_out <= sine_in;
 			else
-				sine_out <= std_logic_vector(one_shift + two_shift + three_shift + four_shift + five_shift + six_shift);
+				sine_out <= std_logic_vector(one_shift_temp + two_shift_temp + three_shift_temp + four_shift_temp + five_shift_temp + six_shift_temp);
 			end if;
 						
 		end if;
 	end process;
+	
+	one_shift_temp <= '0' & one_shift;
+	two_shift_temp <= "00" & two_shift;
+	three_shift_temp <= "000" & three_shift;
+	four_shift_temp <= "0000" & four_shift;
+	five_shift_temp <= "00000" & five_shift;
+	six_shift_temp <= "00000" & six_shift;
 	
 end Behavioral;
 
