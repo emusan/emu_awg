b0fff7d289903a11d79a729b887917303a9a8a91
Tom McLeod <emus.attack@gmail.com>
2014-06-06 16:01:55 -0400

    Change from a two process state machine implementation to a single process. This is far easier to maintain and creates a lot fewer bugs and confusion.

--- src/rotary_control.vhd
+++ src/rotary_control.vhd
@@ -14,75 +14,61 @@ end rotary_control;
 architecture Behavioral of rotary_control is
 
 	type rotary_state is (ablow,abhigh,left,right);
-	signal next_state: rotary_state;
-	signal present_state: rotary_state;
+	signal state: rotary_state;
 
 	signal rotary_input: std_logic_vector(1 downto 0);
 
 	signal direction_sig: std_logic;
 
 begin
-	sync_states: process(clk)
+	process(clk)
 	begin
-		if(rising_edge(clk)) then
-			present_state <= next_state;
-		end if;
-	end process;
-
-	combinatorial: process(present_state,rotary_input)
-	begin
+	if(rising_edge(clk)) then
 		out_pulse <= '0';
 		direction_sig <= direction_sig;
-		case present_state is
+		case state is
 			when ablow =>
-				out_pulse <= '0';
-				direction_sig <= direction_sig;
 				if(rotary_input = "10") then
-					next_state <= left;
+					state <= left;
 				elsif(rotary_input = "01") then
-					next_state <= right; -- maybe left, untested
+					state <= right; -- maybe left, untested
 				else
-					next_state <= ablow;
+					state <= ablow;
 				end if;
 			when abhigh =>
-				out_pulse <= '0';
-				direction_sig <= direction_sig;
 				if(rotary_input = "10") then
-					next_state <= left;
+					state <= left;
 				elsif(rotary_input = "01") then
-					next_state <= right; -- maybe left, untested
+					state <= right; -- maybe left, untested
 				else
-					next_state <= abhigh;
+					state <= abhigh;
 				end if;
 			when left =>
-				out_pulse <= '0';
-				direction_sig <= direction_sig;
 				if(rotary_input = "11") then
 					out_pulse <= '1';
 					direction_sig <= '0';
-					next_state <= abhigh;
+					state <= abhigh;
 				elsif(rotary_input = "00") then
-					next_state <= ablow;
+					state <= ablow;
 				else
-					next_state <= left;
+					state <= left;
 				end if;
 			when right =>
-				out_pulse <= '0';
-				direction_sig <= direction_sig;
 				if(rotary_input = "11") then
 					out_pulse <= '1';
 					direction_sig <= '1';
-					next_state <= abhigh;
+					state <= abhigh;
 				elsif(rotary_input = "00") then
-					next_state <= ablow;
+					state <= ablow;
 				else
-					next_state <= right;
+					state <= right;
 				end if;
 			when others => -- hopefully never happens
-				next_state <= ablow;
+				state <= ablow;
 				out_pulse <= '0';
 				direction_sig <= direction_sig;
 		end case;
+	end if;
 	end process;
 
 	rotary_input <= rotary_a & rotary_b;
